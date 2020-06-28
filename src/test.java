60972036c5d1fd6d3cd8f468c2d4832231ece524
Vadzim PC <comtvset@gmail.com>
2020-06-29 01:50:57 +0200

    add comments

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hellow Alex");
+        System.out.println("Hellow Alex"); //за*бался я с этим гитом!! Это какой-то пиз*ец честно говоря))
     }
 }
